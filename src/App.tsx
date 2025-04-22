fcd37170d1fa64a65a265bce84b584a8ed1c17d4
geotoprz <geotop.rz@gmail.com>
2025-04-22 18:03:01 +0300

    revamp

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import F1TelemetryDashboard from './components/f1teledash'
+
+function App() {
+
+  return (
+    <div className="App">
+      <F1TelemetryDashboard />
+    </div>
+  )
+}
+
+export default App
